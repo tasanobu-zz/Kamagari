3e2a69a96262d642a7ffe301106d2651940f6e80
tasaka-kazunobu <tasaka_kazunobu@cyberagent.co.jp>
2015-06-22 16:42:19 +0900

    Add Xcode project

---
+++ Source/Kamagari.h
@@ -0,0 +1,19 @@
+//
+//  Kamagari.h
+//  Kamagari
+//
+//  Created by Kazunobu Tasaka on 6/22/15.
+//  Copyright (c) 2015 Kazunobu Tasaka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Kamagari.
+FOUNDATION_EXPORT double KamagariVersionNumber;
+
+//! Project version string for Kamagari.
+FOUNDATION_EXPORT const unsigned char KamagariVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Kamagari/PublicHeader.h>
+
+
